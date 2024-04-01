4ca58b7bb95a675d2cee2ccb3b449448fa018670
Ashish <meashish2020@gmail.com>
2024-04-01 19:23:14 +0545

    home

--- booking_UI/resources/views/home.blade.php
+++ booking_UI/resources/views/home.blade.php
@@ -12,19 +12,26 @@
 
         <!-- Full-width images with number and caption text -->
         <div class="mySlides fade">
-          <div class="numbertext">1 / 3</div>
+          <div class="numbertext>">0 / 4</div>
+          <img src="https://lh3.googleusercontent.com/G_3syla0htng_RZlvRq0RrHLtdGPTBcFqE4Fj3m15ipPgdUygujex62Gx98qet95Ahtm-Xj-zs1e-XakiCdPHFqKI48YMIww9TSzl-42WQ" style="width: 100%">
+          <div class="text">Samakushi Futsal</div>
+        </div>
+        <div class="mySlides fade">
+          <div class="numbertext">1 / 4</div>
           <img src="https://1.bp.blogspot.com/-bBgD--rBiOg/Xi7oiO63yOI/AAAAAAAAHi4/MF7YQ_2y3nEArkdIDwOR1GCMvBxpCCeUQCEwYBhgL/s1600/footsal-ground-inside-kathmandu-valley-min.jpg" style="width:100%">
           <div class="text">Highway Futsal</div>
         </div>
       
         <div class="mySlides fade">
-          <div class="numbertext">2 / 3</div>
+          <div class="numbertext">2 / 4</div>
           <img src="https://imageio.forbes.com/specials-images/imageserve/60633c179078ccc3cd7c9e03/0x0.jpg?format=jpg&width=1200" style="width:100%">
           <div class="text">Dhanyentari Futsal</div>
         </div>
       
+        <br>
+        
         <div class="mySlides fade">
-          <div class="numbertext">3 / 3</div>
+          <div class="numbertext">3 / 4</div>
           <img src="https://img.redbull.com/images/c_crop,w_1936,h_968,x_0,y_137,f_auto,q_auto/c_scale,w_1200/redbullcom/2018/07/09/4c915298-2e6b-4f78-8e3f-552407d975bd/futsal-match" style="width:100%">
           <div class="text">Lumbini Futsal</div>
         </div>
